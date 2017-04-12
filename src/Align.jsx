ba8f39e22002d2113803e5f8205d09b6458a9549
Lawrence Dabir-Alai <lawrence.dabir@gmail.com>
2017-04-12 13:34:05 +0100

    Change PropTypes references to prop-types package

--- src/Align.jsx
+++ src/Align.jsx
@@ -1,4 +1,5 @@
-import React, { PropTypes } from 'react';
+import React from 'react';
+import PropTypes from 'prop-types';
 import ReactDOM from 'react-dom';
 import align from 'dom-align';
 import addEventListener from 'rc-util/lib/Dom/addEventListener';
