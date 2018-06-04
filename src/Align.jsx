3d794e1dd068438cd7b2d5984ca2abcc14ca1c3a
zombiej <smith3816@gmail.com>
2018-06-04 17:47:04 +0800

    support point align

--- src/Align.jsx
+++ src/Align.jsx
@@ -1,36 +1,35 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import ReactDOM from 'react-dom';
-import align from 'dom-align';
+import { alignElement, alignPoint } from 'dom-align';
 import addEventListener from 'rc-util/lib/Dom/addEventListener';
 import shallowequal from 'shallowequal';
-import isWindow from './isWindow';
 
-function buffer(fn, ms) {
-  let timer;
+import { isWindow, buffer, isSamePoint } from './util';
 
-  function clear() {
-    if (timer) {
-      clearTimeout(timer);
-      timer = null;
-    }
-  }
+function getElement(func) {
+  if (typeof func !== 'function' || !func) return null;
+  return func();
+}
 
-  function bufferFn() {
-    clear();
-    timer = setTimeout(fn, ms);
-  }
-
-  bufferFn.clear = clear;
-
-  return bufferFn;
+function getPoint(point) {
+  if (typeof point !== 'object' || !point) return null;
+  return point;
 }
 
 class Align extends Component {
   static propTypes = {
     childrenProps: PropTypes.object,
     align: PropTypes.object.isRequired,
-    target: PropTypes.func,
+    target: PropTypes.oneOfType([
+      PropTypes.func,
+      PropTypes.shape({
+        clientX: PropTypes.number,
+        clientY: PropTypes.number,
+        pageX: PropTypes.number,
+        pageY: PropTypes.number,
+      }),
+    ]),
     onAlign: PropTypes.func,
     monitorBufferTime: PropTypes.number,
     monitorWindowResize: PropTypes.bool,
@@ -40,7 +39,6 @@ class Align extends Component {
 
   static defaultProps = {
     target: () => window,
-    onAlign: () => {},
     monitorBufferTime: 50,
     monitorWindowResize: false,
     disabled: false,
@@ -63,11 +61,20 @@ class Align extends Component {
       if (prevProps.disabled || !shallowequal(prevProps.align, props.align)) {
         reAlign = true;
       } else {
-        const lastTarget = prevProps.target();
-        const currentTarget = props.target();
-        if (isWindow(lastTarget) && isWindow(currentTarget)) {
+        const lastElement = getElement(prevProps.target);
+        const currentElement = getElement(props.target);
+        const lastPoint = getPoint(prevProps.target);
+        const currentPoint = getPoint(props.target);
+
+        if (isWindow(lastElement) && isWindow(currentElement)) {
+          // Skip if is window
           reAlign = false;
-        } else if (lastTarget !== currentTarget) {
+        } else if (
+          lastElement !== currentElement || // Element change
+          (!lastElement && currentPoint) || // Change from element to point
+          (!lastPoint && currentElement) || // Change from point to element
+          (currentPoint && !isSamePoint(lastPoint, currentPoint))
+        ) {
           reAlign = true;
         }
       }
@@ -104,10 +111,23 @@ class Align extends Component {
   }
 
   forceAlign = () => {
-    const props = this.props;
-    if (!props.disabled) {
+    const { disabled, target, align, onAlign } = this.props;
+    if (!disabled && target) {
       const source = ReactDOM.findDOMNode(this);
-      props.onAlign(source, align(source, props.target(), props.align));
+
+      let result;
+      const element = getElement(target);
+      const point = getPoint(target);
+
+      if (element) {
+        result = alignElement(source, element, align);
+      } else if (point) {
+        result = alignPoint(source, point, align);
+      }
+
+      if (onAlign) {
+        onAlign(source, result);
+      }
     }
   }
 
